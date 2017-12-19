241ab2b06c7ddab7d8a17404df0c9c6aa60125d4
Nicole E Soltis <ne.soltis@gmail.com>
2017-12-19 14:15:08 -0800

    NES added scripts for gene annotation in R and SNPdat (PERL script)

--- scripts/06_SNPpeaks.R
+++ scripts/06_SNPpeaks.R
@@ -3,115 +3,67 @@
 #---------------------------------------------------------------
 
 rm(list=ls())
+## set correct working directory
 #setwd("~/Documents/GitRepos/BcSolGWAS/")
-setwd("~/../Desktop/B. cinera/Hwaviness/")
+#setwd("~/../Desktop/B. cinera/Hwaviness/")
 
-#Input File: Sl_DomesticationLS_MAF20.HEM.PlotFormat.csv and .Thresh.csv
-#Output File: results/Domestication_TopSNPs_SegLong.csv, results/Domestication_TopSNPs_SegWide.csv
+## update these file names!
+#Input File: data/04_bigRRoutput/trueMAF20_20NA/HWavi_trueMAF20_20NA.HEM.PlotFormat.csv
+#Output File: data/05_genes/Hwavi_TopSNPs_long.csv, data/05_genes/Hwavi_TopSNPs_wide.csv
 #this goes into gene annotation and then venn diagrams
 #Plots: NONE
 ############################################################################
 
 #Load packages
+## install any that are missing
 library(plyr); library(ggplot2); library(grid)
 
-#Import data (reorganized from script ReformatBigRRouts.R)
-HEM.plotdata <- read.csv("data/04_bigRRoutput/trueMAF_10NA/SlBc_domest_trueMAF20_10NA.HEM.PlotFormat.csv")
+#Import data
+## check file names
+HEM.plotdata <- read.csv("data/04_bigRRoutput/trueMAF20_20NA/HWavi_trueMAF20_20NA.HEM.PlotFormat.csv")
 
 #get threshhold values 
-HEM.thresh <- read.csv("data/04_bigRRoutput/trueMAF_10NA/SlBc_domest_trueMAF20_10NA.HEM.Thresh.csv")
+HEM.thresh <- read.csv("data/04_bigRRoutput/trueMAF20_20NA/HWavi_trueMAF20_20NA.HEM.Thresh.csv")
 
 #take the top 50 over the threshold for each phenotype
 TH95pos <- HEM.thresh[1,]
-for (i in 2:ncol(TH95pos)){
-  assign(paste("TH95pos_", names(TH95pos[i]), sep=""),as.numeric(TH95pos[i]))
-}
 TH95neg <- HEM.thresh[5,]
-for (i in 2:ncol(TH95neg)){
-  assign(paste("TH95neg_", names(TH95neg[i]), sep=""),as.numeric(TH95neg[i]))
-}
 TH99pos <- HEM.thresh[3,]
-for (i in 2:ncol(TH99pos)){
-  assign(paste("TH99pos_", names(TH99pos[i]), sep=""),as.numeric(TH99pos[i]))
-}
 TH99neg <- HEM.thresh[7,]
-for (i in 2:ncol(TH99neg)){
-  assign(paste("TH99neg_", names(TH99neg[i]), sep=""),as.numeric(TH99neg[i]))
-}
 TH999pos <- HEM.thresh[4,]
-for (i in 2:ncol(TH999pos)){
-  assign(paste("TH999pos_", names(TH999pos[i]), sep=""),as.numeric(TH999pos[i]))
-}
 TH999neg <- HEM.thresh[8,]
-for (i in 2:ncol(TH999neg)){
-  assign(paste("TH999neg_", names(TH999neg[i]), sep=""),as.numeric(TH999neg[i]))
-}
 
+
+##check if you need to remove first column
+#remove empty first column
 HEM.plotdata <- HEM.plotdata[,-c(1)]
 
 names(HEM.plotdata)
 
 #conditionally replace values < threshold with zero
-HEM.plotdata$Domesticated[HEM.plotdata$Domesticated < TH99pos_Domesticated & HEM.plotdata$Domesticated > 0] <- 0
-HEM.plotdata$Wild[HEM.plotdata$Wild < TH99pos_Wild & HEM.plotdata$Wild > 0] <- 0
-HEM.plotdata$DmWoD[HEM.plotdata$DmWoD < TH99pos_DmWoD & HEM.plotdata$DmWoD > 0] <- 0
-HEM.plotdata$Domesticated[HEM.plotdata$Domesticated > TH99neg_Domesticated & HEM.plotdata$Domesticated < 0] <- 0
-HEM.plotdata$Wild[HEM.plotdata$Wild > TH99neg_Wild & HEM.plotdata$Wild < 0] <- 0
-HEM.plotdata$DmWoD[HEM.plotdata$DmWoD > TH99neg_DmWoD & HEM.plotdata$DmWoD < 0] <- 0
-#remove rows if all 3 = 0 
-HEM.plotdata <- HEM.plotdata[!(HEM.plotdata$Domesticated==0 & HEM.plotdata$Wild==0 & HEM.plotdata$DmWoD==0),]
-#now add counting variable
-HEM.plotdata$TotTraits <- ifelse(abs(HEM.plotdata$Domesticated) >0 & abs(HEM.plotdata$Wild) >0 & abs(HEM.plotdata$DmWoD) >0, "ALL", 
-                              ifelse(abs(HEM.plotdata$Domesticated) >0 & abs(HEM.plotdata$Wild) >0, "DW",
-                                     ifelse(abs(HEM.plotdata$DmWoD) >0 & abs(HEM.plotdata$Wild) >0, "WS",
-                                            ifelse(abs(HEM.plotdata$Domesticated) >0 & abs(HEM.plotdata$DmWoD) >0, "DS",
-                                                   ifelse(abs(HEM.plotdata$Domesticated) >0, "D",
-                                                          ifelse(abs(HEM.plotdata$Wild) >0, "W", "S"))))))
+#for 99% Threshold
+HEM.topSNPs_99Thr <- HEM.plotdata
+HEM.topSNPs_99Thr$Estimate[HEM.topSNPs_99Thr$Estimate < TH99pos$Estimate & HEM.topSNPs_99Thr$Estimate > 0] <- 0
+HEM.topSNPs_999Thr$Estimate[HEM.topSNPs_99Thr$Estimate > TH99neg$Estimate & HEM.topSNPs_99Thr$Estimate < 0] <- 0
+#remove rows if estimate = 0 
+#this keeps only the N rows with SNPs > 99% threshold
+HEM.topSNPs_99Thr <- HEM.topSNPs_99Thr[!(HEM.topSNPs_99Thr$Estimate==0),]
 
-table(HEM.plotdata$TotTraits)
-#All groups (4:6)
-#keep only: SNPs over 99% Threshold
-#now very few over 99.9% Thr
-for (i in 4:6){
-assign(paste("HEMpos.", names(HEM.plotdata[i]), sep=""), subset(HEM.plotdata, HEM.plotdata[i] > get(paste("TH99pos_", names(HEM.plotdata[i]), sep="")), select=c(Chrom,Segment,Pos,Index,i)))
-assign(paste("HEMneg.", names(HEM.plotdata[i]), sep=""), subset(HEM.plotdata, HEM.plotdata[i] < get(paste("TH99neg_", names(HEM.plotdata[i]), sep="")), select=c(Chrom,Segment,Pos,Index,i)))
-}
-
-#for wide format plot need fewer SNPs -- take top 1000 for each? but all of DmWoD
-#top 1000
-HEMpos.Domesticated <- head(arrange(HEMpos.Domesticated, desc(Domesticated)), n=500)
-HEMneg.Domesticated <- tail(arrange(HEMneg.Domesticated, desc(Domesticated)), n=500)
-HEMpos.Wild <- head(arrange(HEMpos.Wild, desc(Wild)), n=500)
-HEMneg.Wild <- tail(arrange(HEMneg.Wild, desc(Wild)), n=500)
-#combine pos and neg by group
-HEM.DmWoD <- rbind(HEMpos.DmWoD, HEMneg.DmWoD)
-HEM.Domesticated <- rbind(HEMpos.Domesticated, HEMneg.Domesticated)
-HEM.Wild <- rbind(HEMpos.Wild, HEMneg.Wild)
-
-
-#then combine
-HEM.Domesticated <- rename(HEM.Domesticated, c("Domesticated" ="Effect"))
-HEM.Domesticated$Trait <- "Domesticated"
-HEM.Wild <- rename(HEM.Wild, c("Wild" ="Effect"))
-HEM.Wild$Trait <- "Wild"
-HEM.DmWoD <- rename(HEM.DmWoD, c("DmWoD" ="Effect"))
-HEM.DmWoD$Trait <- "DmWoD"
-
-HEM.topSNPs <- rbind(HEM.Domesticated, HEM.Wild, HEM.DmWoD)
+#for 99.9% Threshold
+HEM.topSNPs_999Thr <- HEM.plotdata
+HEM.topSNPs_999Thr$Estimate[HEM.topSNPs_999Thr$Estimate < TH999pos$Estimate & HEM.topSNPs_999Thr$Estimate > 0] <- 0
+HEM.topSNPs_999Thr$Estimate[HEM.topSNPs_999Thr$Estimate > TH999neg$Estimate & HEM.topSNPs_999Thr$Estimate < 0] <- 0
+#remove rows if estimate = 0 
+#this keeps only the N rows with SNPs > 99% threshold
+HEM.topSNPs_999Thr <- HEM.topSNPs_999Thr[!(HEM.topSNPs_999Thr$Estimate==0),]
 
 library(ggplot2)
-plot1 <- ggplot(HEM.topSNPs, aes(x=Index, y=Effect))
-plot1 + geom_point(aes(color=factor(Trait)))+
+plot1 <- ggplot(HEM.topSNPs_999Thr, aes(x=Index, y=Estimate))
+plot1 + geom_point()+
   theme_bw()
   
-#make it wide format
-#currently long format : Chrom, Segment, Pos, Index, Effect, Trait
-write.csv(HEM.plotdata, "data/05_annotation/TrueMAF_NAs/Domestication_TopSNPs_SegLong_trueMAF20_10NA.csv")
-write.csv(HEM.topSNPs, "data/05_annotation/TrueMAF_NAs/Domestication_Top1kSNPs_SegLong_trueMAF20_10NA.csv")
+#save it
 
-TopSNP.wide.DM <- reshape(HEM.topSNPs, 
-                         timevar = "Trait",
-                         idvar = c("Chrom","Segment","Pos","Index"),
-                         direction = "wide")
-
-write.csv(TopSNP.wide.DM, "data/05_annotation/TrueMAF_NAs/Domestication_Top1kSNPs_SegWide_trueMAF20_10NA.csv")
+##check these file names
+write.csv(HEM.topSNPs_999Thr, "data/05_genes/Hwavi_TopSNPs999.csv")
+write.csv(HEM.topSNPs_99Thr, "data/05_genes/Hwavi_TopSNPs99.csv")
---
+++ scripts/07_SNPdat_annot.R
@@ -0,0 +1,22 @@
+#Nicole E Soltis
+#format SNP peaks for SNPdat annotation
+#100217
+#-----------------------------------------------------------
+## run this for both TopSNPs999 and TopSNPs99
+rm(list=ls())
+##correct the working directory
+#setwd("~/path/to/dir")
+
+#load files
+myTopSNPs <- read.csv("data/05_genes/Hwavi_TopSNPs999.csv")
+
+#format files for SNPdat 
+names(myTopSNPs)
+myTopSNPs$chromosome.id <- paste("Chromosome",myTopSNPs$Chrom.Cont, sep="")
+myTopSNPs$position <- myTopSNPs$Pos
+myTopSNPs$mutation <- "A"
+myTopSNPs.snpdat <- myTopSNPs[,c("chromosome.id", "position", "mutation")]
+#get rid of ".0"s
+myTopSNPs.snpdat$chromosome.id <- gsub("\\.0$", "", myTopSNPs.snpdat$chromosome.id)
+unique(myTopSNPs.snpdat$chromosome.id)
+write.table(myTopSNPs.snpdat, file="data/05_genes/snpdat/Hwavi_TopSNPs999.FORPERL.txt", sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
