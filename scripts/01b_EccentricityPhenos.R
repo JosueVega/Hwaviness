0946f654aa4a7ed74828ebfa6fc5b4216f15433e
JosueVega <jsvega@ucdavis.edu>
2018-01-12 14:29:12 -0800

    Added template for checking lm and also added model with notes

--- scripts/01b_EccentricityPhenos.R
+++ scripts/01b_EccentricityPhenos.R
@@ -5,7 +5,7 @@
 #prepare lesion eccentricity phenotypes of B. cinerea on A. thaliana for correlation to B. cinerea hyphal waviness
 #-------------------------------------------------------------------------
 rm(list=ls())
-setwd("~/Projects/Hwaviness/data/BcAtPhenos")
+setwd("~/../Desktop/B. cinera/Hwaviness/data/BcAtPhenos/")
 
 isolist <- read.csv("IsolateKey.csv")
 myphenos <- read.csv("BcAt_LesionImageData.csv")
@@ -16,3 +16,23 @@ allphenos <- merge(myphenos,isolist, by="Sample")
 allphenos <- allphenos[,c("Experiment","GrowingFlat","AgarFlat","Plant","Isolate","Lesion.0.m.eccentricity","Lesion.red.m.eccentricity","Lesion.grn.m.eccentricity", "Lesion.blu.m.eccentricity", "Lesion.redgrn.m.eccentricity","Lesion.redblu.m.eccentricity", "Lesion.grnblu.m.eccentricity", "Lesion.Bred.m.eccentricity", "Lesion.Bgrn.m.eccentricity", "Lesion.Bblu.m.eccentricity", "Lesion.Bredgrn.m.eccentricity", "Lesion.Bredblu.m.eccentricity", "Lesion.Bgrnblu.m.eccentricity" )]
 
 write.csv(allphenos, "BcAt_LesionEccentricity.csv")
+View(allphenos)
+
+###### Finding Linear Model 
+
+library(lme4)
+library(lmer)
+#Experiment -> block of experiment in which it was done 
+
+fullmod <- aov(Lesion.0.m.eccentricity ~ Isolate + Plant + Experiment + (Isolate*GrowingFlat) + (Isolate*AgarFlat), allphenos) # all terms included with basic interaction 
+fullmod
+
+
+##Current Most Accurate model with dropped terms
+Cmodel <- lm(Lesion.0.m.eccentricity ~ Isolate + Plant + Experiment + (Isolate*Plant) + (Plant*GrowingFlat) + (Isolate*AgarFlat), allphenos) ## Dropped (Plant*AgarFlat) term b/c change was insignificant 
+
+currmod <- Cmodel # Change for whatever model
+summary(currmod) # provide summary stat over the parameters of the lm
+anova(currmod)# get the anova table for the linear model
+shapiro.test(residuals(currmod))
+
