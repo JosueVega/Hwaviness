5074a768e90e414791c6f2a1df7861aa9866cb2b
JosueVega <jsvega@ucdavis.edu>
2018-02-27 19:49:38 -0800

    Loop working but produces too many values. Needs to be turned into a data frame with adding values in a loop.

--- scripts/00d_Hwavi_Tables.R
+++ scripts/00d_Hwavi_Tables.R
@@ -7,28 +7,25 @@ rm(list=ls())
 library(dplyr)
 library(readr)
 setwd("~/../Desktop/B. cinera/Hwaviness/")
-HwaviBC <- read.csv("WavyGWAS_lsmeans.fxmod1_R_output.csv")
+HwaviBC <- read.csv("../WavyGWAS_lsmeans.fxmod1_R_output.csv") #contains lsmeans instead of average
+HwaviBC <- HwaviBC [,c("Isolate", "HwaviEstimate")] #elimating excess columns for merging
 
-rawTranscripts <- read.csv("../Hwaviness/data/BcAtTranscripts/lsmeans_zscale_allreads.csv")
+rawTranscripts <- read.csv("../Hwaviness/data/BcAtTranscripts/lsmeans_zscale_allreads.csv")#Phentype data (over9k entries)
+
+mergeTranscripts <- merge(HwaviBC, rawTranscripts, by = "Isolate")
 
 BcAt.corr <- NULL
+BcAt.corr <- as.data.frame(BcAt.corr)
+BcAt.corr <-data.frame(mergeTranscripts$Isolate)
+names(BcAt.corr)<-c("p.value","rho.value")
 
-for (i in 1:nrow(AntOverrep)){
-  myrow <- AntOverrep[i,]
-  myA <- myrow$AllWavyGenFreq
-  myB <- myrow$WGGenFreq
-  myCmA <- sum(AntOverrep[,3])-myA
-  myDmB <- sum(AntOverrep$WGGenFreq)-myB
+for (i in 4:ncol(mergeTranscripts)){ #start at 4 since merge scooted it over
+  mycol <- mergeTranscripts[i,]
   
-  PvalueSpea <- cor.test(BcAt_LsWaviness$avg_pheno,BcAt_LsWaviness$anac055, method="spearman", exact=FALSE)$p.value
+  PvalueSpea <- cor.test(mergeTranscripts$HwaviEstimate,mergeTranscripts[[i]], method="spearman", exact=FALSE)$p.value
   PvalueSpea <- format(round(PvalueSpea, 5), nsmall = 4)
-  rhovalueSpea <- cor.test(BcAt_LsWaviness$avg_pheno,BcAt_LsWaviness$anac055, method="spearman", exact=FALSE)$estimate
+  rhovalueSpea <- cor.test(mergeTranscripts$HwaviEstimate,mergeTranscripts[[i]], method="spearman", exact=FALSE)$estimate
   rhovalueSpea <- format(round(rhovalueSpea, 5), nsmall = 4)
-  
-  fisher.p.up <- fisher.test(matrix(c(myA, myB, myCmA, myDmB), nrow=2, ncol=2), alternative="greater")$p.value
-  fisher.p.down <- fisher.test(matrix(c(myA, myB, myCmA, myDmB), nrow=2, ncol=2), alternative="less")$p.value
-  BcAt.corr <- append(BcAt.corr, PvalueSpea)
-  fisher.p.under <- append(fisher.p.under, fisher.p.down)
+
+  BcAt.corr <- paste(BcAt.corr, PvalueSpea)
 }
-assign(paste("fisher.p.over", names(AntOverrep)[3], sep="."),fisher.p.over)
-assign(paste("fisher.p.under", names(AntOverrep)[3], sep="."),fisher.p.under)
